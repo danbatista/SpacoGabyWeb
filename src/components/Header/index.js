4c8d99b1d5ff05e3a425bed334837282271ed0e5
Daniel Batista <dbfilho@brasilseg.com.br>
2020-11-12 09:21:09 -0300

    Subindo o código já desenvolvido

---
+++ src/components/Header/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import Notifications from '~/components/Notifications';
+
+import logo from '~/assets/logo-purple.svg';
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Notifications />
+
+          <Profile>
+            <div>
+              <strong>{profile.name}</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+            <img
+              src={
+                profile.avatar.url ||
+                'https://previews.123rf.com/images/inegvin/inegvin1701/inegvin170100077/69882112-user-sign-icon-person-symbol-human-avatar-.jpg'
+              }
+              alt={profile.name}
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
